ddf2d119b3f701fdf3f5d3812ad02bbf0444bb33
akgamer182 <142451063+akgamer182@users.noreply.github.com>
2024-04-21 21:58:04 -0400

    Added removerole

--- bot.py
+++ bot.py
@@ -142,6 +142,19 @@ async def ban(ctx, id):
         return
     await ctx.reply("You aren't allowed to do this!")
 
+@bot.command()
+async def removerole(ctx, userid, role_name):
+    if ctx.author.id == 357298440650358804: #aks id
+        print(f"demoting user with id {userid}")
+
+        member = await server.fetch_member(userid) 
+        role = get(member.guild.roles, name=role_name)
+        await member.remove_roles(role)
+        await ctx.reply(f"Successfully removed role {role_name} from user {member.name}")
+        return
+        
+    await ctx.reply("You are not allowed to do this!")
+
 @bot.event
 async def on_member_unban(guild: discord.Guild, user: discord.User):
     if user.id == 936408654352101416: #Nick Changer's ID.
